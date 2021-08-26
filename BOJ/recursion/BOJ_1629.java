d5f98562fd0ecd95653ee05072d3f2f181a1067c
Jeongminyooa <jmjmjmz732002@naver.com>
2021-08-26 15:56:02 +0900

    BOJ_1629 solved.

---
+++ BOJ/recursion/BOJ_1629.java
@@ -0,0 +1,39 @@
+package recursion;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_1629 {
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long A, B, C;
+		
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		System.out.println(pow(A, B, C));
+		
+	}
+	
+	//base : ¹Ø, exponent : Áö¼ö
+	static long pow(long base, long exponent, long C) {
+		
+		if(exponent == 1) {
+			return base % C;
+		}
+		
+		long temp = pow(base, exponent/2, C);
+		
+		// Áö¼ö°¡ È¦¼ö¶ó¸é ÇÑ¹ø ´õ ¹ØÀ» °öÇØÁØ´Ù.
+		if(exponent % 2 == 1) {
+			return ((temp * temp % C) * base) % C;
+		}
+		
+		return temp * temp % C;
+	}
+
+}
